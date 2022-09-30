8c7152fb1cdd0f72441cc6a941687678ea67c8e7
Ajay Khatri Chhetri <amajaying@gmail.com>
2022-10-01 03:31:55 +0530

    Longest Palindromic Substring #31
    Hey, This is my first contribution to open source. Thank you for this opportunity :D

--- longest_palindrome_substring.cpp
+++ longest_palindrome_substring.cpp
@@ -1,1 +1,29 @@
-saippuakivikauppias
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int l=0,r=0;
+        for(int mid=0;mid<s.size();++mid){
+            int l1=0,r1=0,l2=0,r2=0;
+            // case 1 mid is mid
+            for(int i=1;mid-i>=0 && mid+i<s.size();++i){
+                if(s[mid-i]!=s[mid+i])
+                    break;
+                l1=mid-i,r1=mid+i;
+            }
+            // case 2 mid is mid-1 & mid+1   
+            for(int i=0;mid-i>=0 && mid+i+1<s.size();++i){
+                if(s[mid-i]!=s[mid+i+1])
+                    break;
+                l2=mid-i,r2=mid+i+1;
+            }
+            
+            //find max length pallindrome of l1,r1 & l2.r2;
+            if( (r1-l1) > (r2-l2) && (r1-l1)> (r-l)){
+                l=l1,r=r1;
+            }else if( (r1-l1) < (r2-l2) && (r2-l2)> (r-l)){
+                l=l2,r=r2;
+            }
+        }        
+        return s.substr(l,r-l+1);
+    }
+};
