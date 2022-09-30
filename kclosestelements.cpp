e32d33ebf2e69f729099b1fe34097acaee8076d6
Subham Sinha <106337171+mrsparkle-70@users.noreply.github.com>
2022-10-01 02:11:38 +0530

    Create kclosestelements.cpp
    Added solution for this problem

---
+++ kclosestelements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size() - k;
+        
+        while(left < right){
+            int mid = (left + right) / 2;
+            if(x-arr[mid] > arr[mid+k]-x){
+                left = mid + 1;
+            }else {
+                right = mid;
+            }
+        }
+        return vector<int>(arr.begin()+left, arr.begin()+left+k);
+    }
+};
