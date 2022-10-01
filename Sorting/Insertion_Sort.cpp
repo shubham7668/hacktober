ceaac6c3040e729ea08f46c1805f6d621781bab9
Aryan Barsaiyan <82722871+AryanBarsaiyan@users.noreply.github.com>
2022-10-01 13:49:40 +0530

    Create Insertion_Sort.cpp

---
+++ Sorting/Insertion_Sort.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertion_sort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    insertion_sort(arr,n);
+    for(int i=0;i<n;i++)
+        cout<<arr[i];
+}
