b48ed95b03b643f7d3ae6186c003cf0608b796cc
AJAY <74255983+AJTEACH@users.noreply.github.com>
2022-10-01 13:04:17 +0530

    Create 2sum.cpp

---
+++ Searching/2sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        //Key is the number and value is its index in the vector.
+        unordered_map<int, int> hash;
+        vector<int> result;
+        for (int i = 0; i < numbers.size(); i++) {
+            int numberToFind = target - numbers[i];
+
+            //if numberToFind is found in map, return them
+            if (hash.find(numberToFind) != hash.end()) {
+                return {hash[numberToFind], i};
+            }
+
+            //number was not found. Put it in the map.
+            hash[numbers[i]] = i;
+        }
+        return result;
+    }
+};
