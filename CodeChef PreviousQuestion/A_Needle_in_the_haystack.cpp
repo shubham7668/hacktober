b0cd269edd32d918df4d9547b9b7a7d807322ec5
Shubham Gaur <94301988+404shubham@users.noreply.github.com>
2022-10-04 12:20:09 +0530

    corrected the solution to the problem
    adding the optimal solution to the problem, solving the question in CPP.

--- CodeChef PreviousQuestion/A_Needle_in_the_haystack.cpp
+++ CodeChef PreviousQuestion/A_Needle_in_the_haystack.cpp
@@ -1,31 +1,19 @@
-// Wrong Solution 
-// Retry link 
-// https://www.hackerearth.com/practice/data-structures/hash-tables/basics-of-hash-tables/practice-problems/algorithm/a-needle-in-the-haystack-1/
+#include <iostream>
 #include<bits/stdc++.h>
-using namespace std; 
-int main(){
-	int t;
-	cin>>t; 
-	while(t--){
-		string s1,s2; 
-		cin>>s1>>s2; 
-		int count = 0; 
-		set<char> ss1,ss2; 
-		for(int i=0;i<s1.size();i++){
-			for(int j=0;j<s2.size();j++){
-				if(s1[i] == s2[j]){
-					s2[j] = '1'; 
-					count++;
-					break;  
-				}
-			}
-		}
-		if(count == s1.size()){
-			cout<<"YES"<<endl; 
-		}
-		else{
-			cout<<"NO"<<endl; 
-		}
-	}
-	return 0; 
+using namespace std;
+int main() {
+    int t,i,j;
+    cin>>t;
+    while(t--)
+    {
+        int c=1;
+        char s[100005],p[1005],p1[1005];
+        cin>>p>>s;
+        char *r1=strstr(s,p);
+        for(i=strlen(p)-1,j=0;i>=0;i--,j++) p1[j]=p[i];
+    char *r2=strstr(s,p1);
+    if(r1||r2)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    }
+    
 }
